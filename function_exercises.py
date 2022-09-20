9f15d3ce078af053e0b45815c236b14fb84426d6
Mark Foster <markdouglasfoster@gmail.com>
2022-09-20 14:53:28 -0500

    Add comments to my functions

--- function_exercises.py
+++ function_exercises.py
@@ -19,22 +19,27 @@ def is_consonant(n):
 # 4. Define a function that accepts a string that is a word. The function should capitalize the first letter of the word if the word starts with a consonant.
 def capitalize_if_consonant(n):
   word = ""
+  # check to see if the first letter is not a vowel. in other words a consonant
   if n[0].lower() not in ("a", "e", "i", "o", "u"):
-    word = n.capitalize()
+    # capitalize the first letter of the words and stores in the variable word as a string
+    print(n.capitalize())
   else:
-    word = n
-  return word
+    #if not a consonant just print out the original word
+    print(n)
 
 # 5. Define a function named calculate_tip. It should accept a tip percentage (a number between 0 and 1) and the bill total, and return the amount to tip.
 def calculate_tip(tip_percent, bill_total):
+  #take in the tip percentage and multiply it by the total bill and round to two decimals and return the value
   return round(tip_percent * bill_total, 2)
 
 # 6. Define a function named apply_discount. It should accept a original price, and a discount percentage, and return the price after the discount is applied.
 def apply_discount(original_price, discount_percentage):
+    #multiply the original price and the discount percentage and subtract that fom the original price, return that
   return round(original_price - (original_price*discount_percentage), 2)
 
 # 7. Define a function named handle_commas. It should accept a string that is a number that contains commas in it as input, and return a number as output.
 def handle_commas(string_number):
+    #take the string passed and take out the commas and return an int
   return int(string_number.replace(',', ''))
 
 # 8. Define a function named get_letter_grade. It should accept a number and return the letter grade associated with that number (A-F).
@@ -55,8 +60,11 @@ def get_letter_grade(num_grade):
 
 # 9. Define a function named remove_vowels that accepts a string and returns a string with all the vowels removed.
 def remove_vowels(words):
+   #check to see if each letter of the word is a vowel 
   for i in ("aeiouAEIOU"):
+    #for each vowel that is in the string, we replace it with nothing
     words = words.replace(i,"")
+  #return the new word without vowels
   return words
 
 
@@ -70,9 +78,19 @@ def remove_vowels(words):
 #    First Name will become first_name
 #    % Completed will become completed
 def normalize_name(word):
+  #make the word all lowercase
   word = word.lower()
+  #take out any leading or tailing space
   word = word.strip()
+  #replace any spaces with _
   word = word.replace(" ", "_")
+  #check if there are any special characters
+  for ch in ['*','{','}','[',']','(',')','>','#','+','-','.','!','$','\'','@','%']:
+        #if there is a special character
+        if ch in word:
+            #replace the special character with nothing
+            word = word.replace(ch,"")
+  #return the string as a valid identifier
   return word
 
 # 11. Write a function named cumulative_sum that accepts a list of numbers 
@@ -80,11 +98,16 @@ def normalize_name(word):
 #cumulative_sum([1, 1, 1]) returns [1, 2, 3]
 #cumulative_sum([1, 2, 3, 4]) returns [1, 3, 6, 10]
 def cumulative_sum(my_list):
+  #create an empty list
   new_list =[]
+  #define x so that we can multiply by it in the for loop
   x = 0
 
+  #take every number in the list
   for i in my_list:
-    x = x + i
+    #take the number from the list and add it to x
+    x += i
+    #add the new x to your list
     new_list.append(x)
-  
+  #return the list of new numbers
   return new_list
