b09bf2e6be2ee20f61b07458d0d34a999baf6381
Mark Foster <markdouglasfoster@gmail.com>
2022-09-21 11:30:39 -0500

    Add some ideas

---
+++ python_data_structure_manipulation_exercises.py
@@ -0,0 +1,51 @@
+###################################################################
+######### 20 Python Data Structure Manipulation Exercises #########
+###################################################################
+
+#1. The following questions reference the students data structure below.
+import students
+
+#2. How many students are there?
+len(students[:])
+
+#3. How many students prefer light coffee? 
+len([c for c in students if c['coffee_preference']=='light'])
+
+#For each type of coffee roast?
+
+
+#4. How many types of each pet are there?
+
+#5. How many grades does each student have? Do they all have the same number of grades?
+
+#6. What is each student's grade average?
+
+#7. How many pets does each student have?
+
+#8. How many students are in web development? data science?
+
+#9. What is the average number of pets for students in web development?
+
+#10. What is the average pet age for students in data science?
+
+#11. What is most frequent coffee preference for data science students?
+
+#12. What is the least frequent coffee preference for web development students?
+
+#13. What is the average grade for students with at least 2 pets?
+
+#14. How many students have 3 pets?
+
+#15. What is the average grade for students with 0 pets?
+
+#16. What is the average grade for web development students? data science students?
+
+#17. What is the average grade range (i.e. highest grade - lowest grade) for dark coffee drinkers?
+
+#18 .What is the average number of pets for medium coffee drinkers?
+
+#What is the most common type of pet for web development students?
+
+#What is the average name length?
+
+#What is the highest pet age for light coffee drinkers?
